d6f9adef808c16821ea862627cedbded6fded04b
noam-steele <99571041+noam-steele@users.noreply.github.com>
2022-04-19 20:45:53 -0700

    Update Player.java

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -2,4 +2,20 @@ package Game;
 
 public class Player {
 
+	//attributes
+	private String name;
+	private String colour;
+	
+	//constructor
+	Player(String name, String colour){
+		this.name = name;
+		this.colour = colour;
+	}
+	//methods
+	public String getName() {
+		return this.name;
+	}
+	public String getColour() {
+		return this.colour;
+	}
 }
